d260020f22319f3eb39a48f69c10b959a58211da
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2024-03-21 17:06:50 -0400

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -4,7 +4,13 @@
 channels=$(rep -Eo 'tvg-id="([^"]+)"' $M3U_FILE | grep -Eo '"([^"]+)"' | tr -d '"')
 
 # put together channels.xml
+echo '<?xml version="1.0" encoding="UTF-8"?>' > ./channels.xml
+echo '<channels>' >> ./channels.xml
 
+# this can't be efficient
+cat ./sites/*/*.channels.xml | grep $channels > ./channels.xml
+
+echo '</channels>' >> ./channels.xml
 
 # start epg
 npm run grab -- --channels=./channels.xml --cron="0 0 * * *" &
