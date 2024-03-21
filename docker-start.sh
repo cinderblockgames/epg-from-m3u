cfe122b7a9795bf9d5a82daa427858f3dbb85ce8
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2024-03-21 18:16:54 -0400

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -4,17 +4,17 @@
 channels=$(grep -Eo 'tvg-id="([^"]+)"' $M3U_FILE | grep -Eo '"([^"]+)"' | tr -d '"')
 
 # put together channels.xml
-echo '<?xml version="1.0" encoding="UTF-8"?>' > $GUIDE_FILE
-echo '<channels>' >> $GUIDE_FILE
+echo '<?xml version="1.0" encoding="UTF-8"?>' > $CHANNELS_FILE
+echo '<channels>' >> $CHANNELS_FILE
 
   # this can't be efficient
-for channel in $channels; do cat ./sites/*/*.channels.xml | grep $channel >> $GUIDE_FILE; done
+for channel in $channels; do cat ./sites/*/*.channels.xml | grep $channel >> $CHANNELS_FILE; done
 
-echo '</channels>' >> $GUIDE_FILE
+echo '</channels>' >> $CHANNELS_FILE
 
 # deduplicate channels.xml
-#awk '!a[$0]++' $GUIDE_FILE > $GUIDE_FILE
+#awk '!a[$0]++' $CHANNELS_FILE > $CHANNELS_FILE
 
 # start epg
-npm run grab -- --channels=$GUIDE_FILE --cron="0 0 * * *" &
+npm run grab -- --channels=$CHANNELS_FILE --output=$GUIDE_FILE --cron="0 0 * * *" &
 npm run serve
